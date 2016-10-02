3abf209bdda3b49b7b1868be92d3430646b67c28
byeongkwan <jbk666@naver.com>
2016-10-02 20:13:58 +0900

    hyperlink

--- app/src/main/java/kr/co/tvtalk/LoginAfterActivity.java
+++ app/src/main/java/kr/co/tvtalk/LoginAfterActivity.java
@@ -2,10 +2,6 @@ package kr.co.tvtalk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-
-import butterknife.ButterKnife;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
