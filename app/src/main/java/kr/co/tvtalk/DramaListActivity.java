ff064c53f446bac2de8ff40eb03ef09a4b0c4dd7
byeongkwan <jbk666@naver.com>
2016-10-02 19:56:41 +0900

    hyperLink add & loginafter layout repair

--- app/src/main/java/kr/co/tvtalk/DramaListActivity.java
+++ app/src/main/java/kr/co/tvtalk/DramaListActivity.java
@@ -6,24 +6,29 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.UnderlineSpan;
 import android.view.View;
 import android.widget.TextView;
-
-import kr.co.tvtalk.R;
-import kr.co.tvtalk.activitySupport.FontFactory;
-import kr.co.tvtalk.activitySupport.dramalist.DramaListAdapter;
-import kr.co.tvtalk.activitySupport.dramalist.DramaData;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import kr.co.tvtalk.activitySupport.FontFactory;
+import kr.co.tvtalk.activitySupport.dramalist.DramaData;
+import kr.co.tvtalk.activitySupport.dramalist.DramaListAdapter;
 
 /**
  * Created by kwongyo on 2016-09-16.
  */
     public class DramaListActivity extends AppCompatActivity {
+
+        @Bind(R.id.broadcast_link)
+        TextView broadLink;
 
         @Bind(R.id.mdl_recycler)
         RecyclerView mdlRcycler;
@@ -60,9 +65,26 @@ import butterknife.OnClick;
                                 R.drawable.icon_clock
                         ));
             }
+            text_concept();                 //하이퍼링크
         }
-        @OnClick(R.id.mdl_back_btn)
+        // 하이퍼링크
+        private void text_concept() {
+            Spanned result;
+            SpannableString content1 = new SpannableString("MBC 다시보기");
+            content1.setSpan(new UnderlineSpan(),0,content1.length(),0);
+            broadLink.setText(content1);
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {     //sdk 24 일때
+                broadLink.setText(Html.fromHtml("<a href=\"http://www.imbc.com\">MBC</a>", Html.FROM_HTML_MODE_LEGACY));
+            }
+            else                                                                            //sdk 24 아닐때
+                broadLink.setText(Html.fromHtml("<a href=\"http://www.imbc.com\">MBC</a>"));
+
+        }
+
+    @OnClick(R.id.mdl_back_btn)
         public void mdlBackBtn(View v) {
             finish();
         }
+
+
 }
--- app/src/main/java/kr/co/tvtalk/LoginAfterActivity.java
+++ app/src/main/java/kr/co/tvtalk/LoginAfterActivity.java
@@ -1,27 +1,53 @@
 package kr.co.tvtalk;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import kr.co.tvtalk.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by user on 2016-09-16.
  */
 public class LoginAfterActivity extends AppCompatActivity {
 
-    @Bind(R.id.Loginafter_image)CircleImageView circle_Image;
-    @Bind(R.id.loginafter_name)TextView loginafter_name;
-    @Bind(R.id.loginafter_email_id)EditText loginafter_email;
-    @Bind(R.id.loginafter_password)EditText loginafter_pw;
+    @Bind(R.id.profile_image)
+    ImageView profileImage;
+
+    @Bind(R.id.profile_name)
+    TextView profileName;
+
+    @Bind(R.id.name_repair)
+    ImageView nameRepair;
+
+    @Bind(R.id.login_email_id)
+    EditText loginEmailID;
+
+    @Bind(R.id.login_password)
+    EditText loginPassword;
+
+    @Bind(R.id.login_confirm)
+    EditText loginConfirm;
+
+    @Bind(R.id.login_repair_btn)
+    Button loginRepairBtn;
+    @OnClick(R.id.login_repair_btn)
+    public void loginRepairClick(View v){
+
+    }
+
+    @Bind(R.id.logout_btn)
+    Button logoutBtn;
+    @OnClick(R.id.logout_btn)
+    public void logoutClick(View v){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +55,4 @@ public class LoginAfterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loginafter);
         ButterKnife.bind(this);
     }
-
-    /*@OnClick
-    public void loginafter_change(View v){
-        Intent intent_pw = new Intent(this,ChangeActivity.class);
-        startActivity(intent_pw);
-    }
-
-    @OnClick
-    public  void loginafter_logout(View v){
-
-    }*/
 }
